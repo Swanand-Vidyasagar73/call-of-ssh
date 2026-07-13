8172ffdb6596f72e1001baa79714364540d6adc5
Swanand <swanandvsagar@gmail.com>
2026-07-14 01:17:28 +0530

    Added identity and main python files and completed ssh_client

--- client/ssh_client.py
+++ client/ssh_client.py
@@ -10,3 +10,14 @@ def open_shell_channel(client):
     channel = client.invoke_shell()
     return channel
 
+if __name__ == "__main__":
+    client = connect("127.0.0.1", 2222, "swanand", "keys/alice_id_ed25519")
+    chan = open_shell_channel(client)
+
+    chan.send("echo hello-over-ssh\n")
+    import time
+    time.sleep(1)
+    print(chan.recv(4096).decode())
+
+    client.close()
+
